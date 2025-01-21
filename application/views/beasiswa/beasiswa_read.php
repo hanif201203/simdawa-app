9dd731b49f44043d7c336a21aba81c8350aa37fb
hanif201203 <hanif20122003@gmail.com>
2025-01-21 20:19:50 +0700

    “Membuat halaman pendaftaran, login dan pembatasan akses”

--- application/views/beasiswa/beasiswa_read.php
+++ application/views/beasiswa/beasiswa_read.php
@@ -23,8 +23,11 @@
                 ?>
                 <div class="card">
                     <div class="card-header">
+                        Data Beasiswa
+                        <?php if ($this->session->userdata('peran' != 'USER')): ?>
                         <a href="<?= base_url('beasiswa/tambah') ?>" class="btn btn-sm btn-success float-right"><i class="fas fa-plus">Tambah Data</i></a>
                         <a href="<?= base_url('beasiswa/cetak') ?>" target="_blank" class="btn btn-sm btn-info me-1 float-right"><i class="fas fa-print">Cetak Data</i></a>
+                        <?php endif; ?>
                     </div>
                     <div class="card-body">
                         <table class="table table-bordered" id="mytabel">
@@ -35,7 +38,9 @@
                                     <th>Tanggal Mulai</th>
                                     <th>Tanggal Selesai</th>
                                     <th>Jenis Beasiswa</th>
+                                    <?php if ($this->session->userdata('peran' != 'USER')): ?>
                                     <th>Aksi</th>
+                                    <?php endif;?>
                                 </tr>
                             </thead>
                             <tbody>
@@ -47,10 +52,12 @@
                                         <td><?= $b->tanggal_mulai ?></td>
                                         <td><?= $b->tanggal_selesai ?></td>
                                         <td><?= $b->nama_jenis ?></td>
+                                        <?php if ($this->session->userdata('peran' != 'USER')): ?>
                                         <td>
                                             <a href="<?= base_url('beasiswa/ubah/' . $b->id) ?>" class="btn btn-sm btn-info"><i class="fas fa-edit"></i> Ubah</a>
                                             <a href="<?= base_url('beasiswa/hapus/' . $b->id) ?>" class="btn btn-sm btn-danger" onclick="return confirm('Ingin hapus data ini?')"><i class="fas fa-trash"></i> Hapus</a>
                                         </td>
+                                        <?php endif; ?>
                                     </tr>
                                 <?php endforeach; ?>
                             </tbody>
